bbae0f8904bf7354f745fc99651eaa522635001d
Mateo Garcia <89867965+MateoGP00@users.noreply.github.com>
2024-11-03 21:00:43 -0300

    Update main.c

--- codigo/main.c
+++ codigo/main.c
@@ -1,1 +1,8 @@
-
+#include "conf.h"
+int main (){
+	estados_t estado = botonDeHabilitacion;
+	estados_t (*funcion[3])(void) = {f_botonDeHabilitacion, f_puertaCerrada, f_puertaAbierta};
+	init_mcu();
+	while(1) estado = (*funcion[estado])();
+	return 0;
+}
